47ecaa04adb6ef77be4170cdb61e7b77e85b2e7f
aurimas-darguzis <aurimas@darguzis.com>
2017-04-08 09:57:39 +0100

    first commit

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,29 +1,14 @@
-var div = React.DOM.div
-var h1 = React.DOM.h1
-
-var MyTitle = React.createClass({
-  render () {
-    return (
-      div(null,
-         h1({ style: { color: this.props.color, fontWeight: 'bold' } }, this.props.title)
-      )
-    )
-  }
-})
-
-var MyTitleFact = React.createFactory(MyTitle)
-
-
-var MyFirstComponent = React.createClass({
-  render () {
-    return (
-      div(null,
-        MyTitleFact({title: 'Props are great!', color: 'peru'}),
-        MyTitleFact({title: 'Use props everywhere!', color: 'mediumquamarine'}),
-        MyTitleFact({title: 'Props are the best!', color: 'rebeccapurple'})
-      )
-    )
-  }
-})
-
-ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
+      var div = React.DOM.div
+      var h1 = Reac.DOM.h1
+      
+      var MyFirstComponent = React.createClass({
+        render: function() {
+          return (
+            div(null,
+              h1(null, 'This is my First Component!')
+            )
+          )
+        }
+      })
+      
+      ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
