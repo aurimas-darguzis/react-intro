3d4e1faa543ebc4fe1ef3538b9f39ac9b449d3b0
aurimas-darguzis <aurimas@darguzis.com>
2017-04-08 10:30:05 +0100

    linted with standard

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,30 +1,32 @@
-      var div = React.DOM.div
-      var h1 = React.DOM.h1
+/* global React ReactDOM */
 
-      var MyTitle = React.createClass({
-          render: function () {
-              return (
-                  div(null,
-                    h1(null, this.props.title) // 'this' reffers to var MyTitle = React.createClass'
-                                               // 'props' reffers to arguments passed by parent;
-                  )
-              )
-          }
-      })
+var div = React.DOM.div
+var h1 = React.DOM.h1
 
-      var MyTitleFactory = React.createFactory(MyTitle)
-      
-      var MyFirstComponent = React.createClass({
-        render: function() {
-          return (
-            div(null,
-              MyTitleFactory({ title: 'props are the best' }),
-              MyTitleFactory({ title: 'semicolons are the wors' }),
-              MyTitleFactory({ title: 'its okay if you like semicolons' }),
-              MyTitleFactory({ title: 'im out of ideas '})
-            )
-          )
-        }
-      })
-      
-      ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
+var MyTitle = React.createClass({
+  render: function () {
+    return (
+      div(null,
+        h1({ style: { color: this.props.color } }, this.props.title) // 'this' reffers to var MyTitle = React.createClass'
+                                        // 'props' reffers to arguments passed by parent;
+      )
+    )
+  }
+})
+
+var MyTitleFactory = React.createFactory(MyTitle)
+
+var MyFirstComponent = React.createClass({
+  render: function () {
+    return (
+      div(null,
+        MyTitleFactory({ title: 'props are the best', color: 'peru' }),
+        MyTitleFactory({ title: 'semicolons are the wors', color: 'mediumaquamarine' }),
+        MyTitleFactory({ title: 'its okay if you like semicolons', color: 'rebeccapurple' }),
+        MyTitleFactory({ title: 'im out of ideas ', colors: 'darkvioletred' })
+      )
+    )
+  }
+})
+
+ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
