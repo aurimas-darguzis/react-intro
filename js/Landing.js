c2105e7035becb11e746d9d8aa7e93a004178a05
aurimas-darguzis <aurimas@darguzis.com>
2017-04-12 20:48:20 +0100

    dispatching action to the root reducer

--- js/Landing.js
+++ js/Landing.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
-const { string } = React.PropTypes
+import { setSearchTerm } from './actionCreators'
+const { string, func } = React.PropTypes
 
 const Landing = React.createClass({
   // this is comming from redux via connect
   propType: {
-    searchTerm: string
+    searchTerm: string,
+    dispatch: func
+  },
+  handleSearchTermChange (event) {
+    // dispatch comes from connect
+    this.props.dispatch(setSearchTerm(event.target.value))
   },
   render () {
     return (
       <div className='landing'>
         <h1>svideo</h1>
-        <input value={this.props.searchTerm} type='text' placeholder='search' />
+        <input onChange={this.handleSearchTermChange} value={this.props.searchTerm} type='text' placeholder='search' />
         <Link to='/search'>or Browse All</Link>
       </div>
     )
