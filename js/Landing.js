36b7b8eb959f209dfdb726e11903e89ee9f8b509
aurimas-darguzis <aurimas@darguzis.com>
2017-04-12 20:31:44 +0100

    getting state from redux on the UI

--- js/Landing.js
+++ js/Landing.js
@@ -1,16 +1,29 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router'
+const { string } = React.PropTypes
 
 const Landing = React.createClass({
+  // this is comming from redux via connect
+  propType: {
+    searchTerm: string
+  },
   render () {
     return (
       <div className='landing'>
         <h1>svideo</h1>
-        <input type='text' placeholder='search' />
+        <input value={this.props.searchTerm} type='text' placeholder='search' />
         <Link to='/search'>or Browse All</Link>
       </div>
     )
   }
 })
 
-export default Landing
+// pull from redux and pass as a prop
+const mapStateToProps = (state) => {
+  return {
+    searchTerm: state.searchTerm
+  }
+}
+
+export default connect(mapStateToProps)(Landing)
