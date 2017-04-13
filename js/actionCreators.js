c1c46f80b1522abe04f3411720854698f73c9cf5
aurimas-darguzis <aurimas@darguzis.com>
2017-04-13 21:18:40 +0100

    added action creators

--- js/actionCreators.js
+++ js/actionCreators.js
@@ -1,5 +1,9 @@
-import { SET_SEARCH_TERM } from './actions'
+import { SET_SEARCH_TERM, ADD_OMDB_DATA } from './actions'
 
 export function setSearchTearm (search) {
   return { type: SET_SEARCH_TERM, searchTerm } // returns correctly shaped action
+}
+
+export function addOMDBData (imdbID, omdbData) {
+  return { type: ADD_OMDB_DATA, imdbID, omdbData}
 }
