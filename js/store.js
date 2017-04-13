1d6d01ccc8668873e4232ab12a1ab5626269b85a
aurimas-darguzis <aurimas@darguzis.com>
2017-04-13 20:41:56 +0100

    full redux implementation

--- js/store.js
+++ js/store.js
@@ -1,6 +1,9 @@
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import rootRedcuer from './reducers'
 
-const store = createStore(rootRedcuer)
+const store = createStore(rootRedcuer, compose(
+  typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ?
+    window.devToolsExtension() : (f) => f
+))
 
 export default store
