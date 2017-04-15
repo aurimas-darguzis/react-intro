e4d6ce672b78c70e772fbd6623b7f24aa62a6283
aurimas-darguzis <aurimas@darguzis.com>
2017-04-15 23:01:57 +0100

    learning by doing

---
+++ js/LandingAxioma.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LandingAxioma = React.createClass({
+  render () {
+    return (
+      <div>
+        <h1>Axioma World</h1>
+        <input type='text' placeholder='Search' />
+        <a>or Browse All</a>
+      </div>
+    )
+  }
+})
+
+export default LandingAxioma
